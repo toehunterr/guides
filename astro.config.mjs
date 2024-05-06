923bd21b8416bd43498ad65248cae9d0c70ee20a
pedrotski <58076186+pedrotski@users.noreply.github.com>
2024-05-06 15:50:20 +0800

    Add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
       baseUrl: 'https://github.com/Game-Host-Bros/guides/edit/main/',
     },
     customCss: ['./src/styles/custom.css'],
-    favicon: './src/assets/dark-logo.png',
+    favicon: 'favicon.ico',
     logo: {
       light: './src/assets/light-logo.png',
       dark: './src/assets/dark-logo.png',
